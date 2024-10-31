bde235fd7c7dbf7b97a1bd11e21ba483edd54bf6
HoangQuocDuy29 <quocduyhoang42@gmail.com>
2024-10-31 10:30:58 +0700

    Add project files.

---
+++ Nhom2/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Nhom2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.btndn = new System.Windows.Forms.Button();
+            this.btnthoat = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(199, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(398, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ĐĂNG NHẬP HỆ THỐNG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "User : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(204, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password :";
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(207, 153);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(169, 22);
+            this.txtuser.TabIndex = 3;
+            this.txtuser.TextChanged += new System.EventHandler(this.txtuser_TextChanged);
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(207, 248);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(169, 22);
+            this.txtpassword.TabIndex = 4;
+            this.txtpassword.TextChanged += new System.EventHandler(this.txtpassword_TextChanged);
+            // 
+            // btndn
+            // 
+            this.btndn.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndn.Location = new System.Drawing.Point(178, 334);
+            this.btndn.Name = "btndn";
+            this.btndn.Size = new System.Drawing.Size(144, 37);
+            this.btndn.TabIndex = 5;
+            this.btndn.Text = "Đăng Nhập";
+            this.btndn.UseVisualStyleBackColor = true;
+            this.btndn.Click += new System.EventHandler(this.btndn_Click);
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnthoat.Location = new System.Drawing.Point(382, 334);
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(138, 37);
+            this.btnthoat.TabIndex = 6;
+            this.btnthoat.Text = "Thoát";
+            this.btnthoat.UseVisualStyleBackColor = true;
+            this.btnthoat.Click += new System.EventHandler(this.btnthoat_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnthoat);
+            this.Controls.Add(this.btndn);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "FrmDangNhap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Button btndn;
+        private System.Windows.Forms.Button btnthoat;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Nhom2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+using System.Windows.Forms;
+
+namespace Nhom2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtuser_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtpassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btndn_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=HOANG-QUOC-DUY\\SQLEXPRESS;Initial Catalog=Nhom2;Integrated Security=True;"; // Chuỗi kết nối đến SQL Server
+            string query = "SELECT COUNT(*) FROM NguoiDung WHERE UserName = @username AND Password = @password";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@username", txtuser.Text);
+                command.Parameters.AddWithValue("@password", txtpassword.Text);
+
+                try
+                {
+                    connection.Open();
+                    int result = (int)command.ExecuteScalar();
+
+                    if (result > 0)
+                    {
+                        // Đăng nhập thành công, chuyển sang form FrmMenu
+                        FrmMenu menuForm = new FrmMenu();
+                        menuForm.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        // Đăng nhập thất bại
+                        MessageBox.Show("Bạn đã nhập sai thông tin đăng nhập", "Lỗi đăng nhập", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Có lỗi xảy ra: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
